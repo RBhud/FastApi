b7d7a924d4e8cb0fb797cb8e30a44bce73f3bf75
Rushil Bhudia <zlac200@live.rhul.ac.uk>
2025-08-07 22:12:22 +0100

    implemented the /summary/{user_id} endpoint to retrieve the minimum, maximum and mean transaction amounts.

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
+from datetime import datetime
 import csv
 import os
 
+class Filename:
+    def __init__(self):
+        self.filename = None
+
 app = FastAPI()
+# store the most recent uploaded filename
+uploaded_file = Filename()
 
 # localhost:8000/upload endpoint
 # Upload file to endpoint and check CSV validity
@@ -31,8 +38,71 @@ async def upload_csv(file: UploadFile = File(...)):
     # build the path
     file_location = os.path.join(current_dir, file.filename)
 
+    # update the uploaded file ready for processing
+    uploaded_file.filename = file.filename
+
     # store the file in the current directory
     with open(file_location, 'wb') as f:
         f.write(content)
 
     return {'msg' : "File Uploaded."}
+
+# localhost:8000/summary/<user_id> endpoint
+# get the summary of the user with the requested id 
+@app.get("/summary/{user_id}")
+async def get_summary(user_id: int):
+    
+    # check a file has been uploaded
+    if uploaded_file.filename is None:
+        raise HTTPException(status_code=404, detail="No files uploaded.")
+
+    # initialise values for loop
+    minimum = 1000000
+    maximum = 0
+    total_transaction_amount = 0
+    record_count = 0
+    minimum_date = ""
+    maximum_date = ""
+
+    # open the most recently uploaded file
+    with open(uploaded_file.filename) as file:
+
+        # read the csv file into lists of dictionaries
+        reader = csv.DictReader(file)
+
+        transaction_amount = 0
+        # loop through all records belonging to the requested user
+        for row in reader:
+
+            if int(row.get("user_id")) == user_id:
+
+                # get the minimum and maximum transaction amount
+                transaction_amount = float(row.get("transaction_amount"))
+                minimum = check_min(minimum, transaction_amount)
+                maximum = check_max(maximum, transaction_amount)
+                total_transaction_amount += transaction_amount
+                record_count += 1
+
+        # check if records are found for the user id
+        if record_count == 0:
+            raise HTTPException(status_code=404, detail=f"No records found for user id {user_id}")
+        
+        # calculate mean of all the users' transactions
+        mean = total_transaction_amount / record_count
+
+    return {"min": f"{minimum}", "max": f"{maximum}", "mean": f"{mean}"}
+
+# check for minimum transaction amount
+def check_min(current, check_val):
+    if current < check_val:
+        return current
+    else:
+        return check_val
+
+# check for maximum transaction amount
+def check_max(current, check_val):
+    if current > check_val:
+        return current
+    else:
+        return check_val
+   
--- test_main.py
+++ test_main.py
@@ -1,11 +1,17 @@
 from fastapi.testclient import TestClient
 
-from .main import app
+from .main import app, uploaded_file
 
+# Create the test client
 client = TestClient(app)
 
+# test valid file uploads
 def test_upload():
+    
+    # temporarily hold the uploaded file name if server is running
+    temp = uploaded_file.filename
 
+    # upload new csv file
     response = client.post(
         "/upload",
         files={"file": ("sample.csv", "sample data", "text/csv")},
@@ -14,8 +20,13 @@ def test_upload():
     assert response.status_code == 200
     assert response.json() == {"msg": "File Uploaded."}
 
+    # reassign the file name
+    uploaded_file.filename = temp
+
+# test invalid file uploads
 def test_invalid_upload():
 
+    # upload invalid text file 
     response = client.post(
         "/upload",
         files={"file": ("sample.txt", "sample text", "text/plain")},
@@ -24,6 +35,7 @@ def test_invalid_upload():
     assert response.status_code == 400
     assert response.json() == {"detail": "Only CSV files are accepted."}
 
+    # upload file with invalid content type
     response = client.post(
         "/upload",
         files={"file": ("sample.csv", "sample text", "text/plain")},
@@ -31,3 +43,53 @@ def test_invalid_upload():
 
     assert response.status_code == 400
     assert response.json() == {"detail": "Only 'text/csv' files are accepted."}
+
+# test no file uploaded
+def test_no_file():
+
+    # temporarily set the uploaded file name to None
+    temp = uploaded_file.filename
+    uploaded_file.filename = None
+
+    response = client.get("/summary/1")
+
+    assert response.status_code == 404
+    assert response.json() == {"detail": "No files uploaded."}
+
+    # reassign the file name
+    uploaded_file.filename = temp
+    
+
+# test invalid user id
+def test_invalid_user_id():
+
+    # temporarily set the uploaded file name to dummy transactions
+    temp = uploaded_file.filename
+    uploaded_file.filename = "dummy_transactions.csv"
+
+    response = client.get("/summary/1001")
+
+    assert response.status_code == 404
+    assert response.json() == {"detail" : "No records found for user id 1001"}
+
+    # reassign the file name
+    uploaded_file.filename = temp
+
+# test valid user id
+def test_valid_user_id():
+
+    # temporarily set the uploaded file name to dummy transactions
+    temp = uploaded_file.filename
+    uploaded_file.filename = "dummy_transactions.csv"
+
+    response = client.get("/summary/289")
+
+    assert response.status_code == 200
+    assert response.json() == {
+                            "min": "5.12",
+                            "max": "498.48",
+                            "mean": "243.16170731707314"
+                            }
+
+    # reassign the file name
+    uploaded_file.filename = temp
